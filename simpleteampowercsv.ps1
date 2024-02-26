9edfcefea61257f8bfb57f7c081ea4fb90c65fb6
Vasileios Tzimourtos <billtzim@yahoo.gr>
2024-02-26 17:12:51 +0200

    changed the save paths to new ones

--- simpleteampowercsv.ps1
+++ simpleteampowercsv.ps1
@@ -1,8 +1,18 @@
-﻿Connect-MicrosoftTeams
+﻿#2) Για όσους επιθυμούν να κάνουν ενέργειες μέσω του Windows PowerShell, παραθέτω μερικές αρχικές πληροφορίες:
+#- Θα χρειαστεί να εγκαταστήσετε το module: MicrosoftTeams (https://www.powershellgallery.com/packages/MicrosoftTeams/)
+#- Κάνετε connect-MicrosoftTeams με τον λογαριασμό teleconf....@o365.uth.gr
+#- Κατόπιν διαχειρίζεστε τις ομάδες Teams στις οποίες είστε Owner, με εντολές του module: https://docs.microsoft.com/en-us/powershell/module/teams/?view=teams-ps
+#ΕΦΙΣΤΩ ΠΡΟΣΟΧΗ στη χρήση εντολών που δεν ξεκινούν από Get- (πχ. Add- Set- , Remove- κτλ) καθώς επεμβαίνουν σε ρυθμίσεις στα Teams.
+#Οι εντολές Get- έχουν χαρακτήρα "Read-Only", πχ.:
+#get-Team -User teleconf....@o365.uth.gr | Format-Table -Property DisplayName
+#ή
+#Get-Team | Group-Object description | Format-Table -Property groupid,displayname,description -AutoSize | Out-File -FilePath c:\UThMsTeams.txt
+
+Connect-MicrosoftTeams
 
 # Define the path to your CSV file
-#$csvFilePath = "d:\tests\powercsv\simple_courses_codes_econ.csv"
-$csvFilePath = "simple_courses_codes_econ.csv"
+$csvFilePath = "D:\Repositories\powerteams\simple_courses_codes_econ.csv"
+#$csvFilePath = "simple_courses_codes_econ.csv"
 
 # Read the CSV file
 $data = Import-Csv -Path $csvFilePath
@@ -22,6 +32,7 @@ foreach ($row in $data) {
     # Construct the command with parameters
     #$fullCommand = "$commandToExecute -Title '$title' -Description '$description' -Owner '$owner'"
     $group = New-Team -DisplayName $title -Description $description -AllowGiphy $false -AllowDeleteChannels $false -AllowCreateUpdateRemoveTabs $false -AllowCreateUpdateRemoveConnectors $false -AllowCreateUpdateChannels $false -AllowAddRemoveApps $false
+    Add-TeamUser -GroupId $group.GroupId -User "vtzimourtos@o365.uth.gr" -Role Owner
 
     # Execute the command
     #Write-Host "Executing command: $fullCommand"
@@ -40,7 +51,8 @@ foreach ($row in $data) {
 }
 
 # Define the path to save the results CSV file
-$resultsFilePath = "SAVED_RESULTS_simple_courses_codes_econ.csv"
+$resultsFilePath = "D:\Repositories\powerteams\SAVED_RESULTS_simple_courses_codes_econ.csv"
+#$resultsFilePath = "SAVED_RESULTS_simple_courses_codes_econ.csv"
 
 # Export results to a CSV file
 $results | Export-Csv -Path $resultsFilePath -NoTypeInformation
